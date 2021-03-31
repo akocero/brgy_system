6055c1400a42a4d8ed0f99a95c04c401b61de247
utoy sakalam <badatoeugenepaulm@gmail.com>
2021-04-01 00:53:23 +0800

    setup resident table

--- database/migrations/2021_03_31_153359_create_residents_table.php
+++ database/migrations/2021_03_31_153359_create_residents_table.php
@@ -15,6 +15,22 @@ class CreateResidentsTable extends Migration
     {
         Schema::create('residents', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('middle_name')->nullable();
+            $table->string('suffix')->nullable();
+            $table->text('address');
+            $table->string('gender');
+            $table->string('mobile_no');
+            $table->string('landine')->nullable();
+            $table->string('email')->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->date('place_of_birth')->nullable();
+            $table->string('occupation')->nullable();
+            $table->string('citizenship')->nullable();
+            $table->string('civil_status');
+            $table->integer('active')->default(1);
+            $table->string('is_voter')->default(false);
             $table->timestamps();
         });
     }
